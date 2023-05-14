cb00ed7a2895d4744e66a53f392ce5bba1805daa
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-05-14 12:19:49 -0400

    refactor for modified type

--- src/app/modules/clients/services/client.service.ts
+++ src/app/modules/clients/services/client.service.ts
@@ -5,14 +5,14 @@ import { Client } from '../interfaces/client';
 const clients: Client[] = [
   {
     id: '1',
-    name: 'Deloitte',
+    company: 'Deloitte',
     contactName: 'Mark Andressen',
     contactEmail: 'm.andressen@deloitte.com',
     createdAt: 'Today',
   },
   {
     id: '2',
-    name: 'Ernst & Young',
+    company: 'Ernst & Young',
     contactName: 'Joshua Carlston',
     contactEmail: 'jcarl@ey.com',
     createdAt: 'Yesterday',
