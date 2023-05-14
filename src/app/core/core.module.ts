0ca086319afafd9aacfda96d2093195b2fcb413d
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-05-14 12:15:13 -0400

    import browser animations module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [SidebarComponent],
   exports: [SidebarComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, BrowserAnimationsModule],
 })
 export class CoreModule {}
