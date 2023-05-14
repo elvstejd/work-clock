c7c1ff10c4bb03200d471ffc0872cb08f20c8f78
Elvis Tejeda <elvistejeda0910@gmail.com>
2023-05-14 12:48:21 -0400

    redesign sidebar with tailwind

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-
+  links = [
+    {
+      label: 'Time tracker',
+      path: '/',
+    },
+    {
+      label: 'Projects',
+      path: '/projects',
+    },
+    {
+      label: 'Clients',
+      path: '/clients',
+    },
+  ];
 }
